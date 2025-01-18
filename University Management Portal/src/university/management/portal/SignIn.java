944a307741ee4a87a39ea2810fdb7ba25d95151b
SRINIKETH VENKATESH <120231325+Sriniketh010597@users.noreply.github.com>
2025-01-17 21:52:25 -0500

    Add files via upload

---
+++ University Management Portal/src/university/management/portal/SignIn.java
@@ -0,0 +1,83 @@
+package university.management.portal;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+
+public class SignIn extends JFrame implements ActionListener{
+
+    JButton signin, exit;
+    JTextField signinusername, signinpassword;
+    
+    SignIn () {
+        
+        getContentPane().setBackground(Color.WHITE);
+        setLayout(null);
+        
+        JLabel lblusername = new JLabel("Username");
+        lblusername.setBounds(45, 25, 105, 25);
+        add(lblusername);
+        
+        signinusername = new JTextField();
+        signinusername.setBounds(155, 25, 155, 25);
+        add(signinusername);
+        
+        JLabel lblpassword = new JLabel("Password");
+        lblpassword.setBounds(45, 75, 105, 25);
+        add(lblpassword);
+        
+        signinpassword = new JPasswordField();
+        signinpassword.setBounds(155, 75, 155, 25);
+        add(signinpassword);
+        
+        signin = new JButton("signin");
+        signin.setBounds(40, 140, 120, 30);
+        signin.setBackground(Color.BLACK);
+        signin.setForeground(Color.WHITE);
+        signin.addActionListener(this);
+        signin.setFont(new Font("Calibre", Font.BOLD, 15));
+        add(signin);
+        
+        exit = new JButton("exit");
+        exit.setBounds(180, 140, 120, 30);
+        exit.setBackground(Color.BLACK);
+        exit.setForeground(Color.WHITE);
+        exit.addActionListener(this);
+        exit.setFont(new Font("Calibre", Font.BOLD, 15));
+        add(exit);
+        
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/second.jpg"));
+        Image i2 = i1.getImage().getScaledInstance(200, 200, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel image = new JLabel(i3);
+        image.setBounds(350, 0, 200, 200);
+        add(image);
+        
+        setSize(610, 315);
+        setLocation(510, 260);
+        setVisible(true);
+    }
+    
+    public void actionPerformed(ActionEvent ae) {
+        if (ae.getSource() == signin) {
+            
+            
+            try {
+                
+                
+                
+          
+                
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else if (ae.getSource() == exit) {
+            setVisible(false);
+        }
+    }
+
+    public static void main(String[] args) {
+        new SignIn();
+    }
+}
---
+++ University Management Portal/src/university/management/portal/WelcomeScreen.java
@@ -0,0 +1,47 @@
+package university.management.portal;
+
+import javax.swing.*; 
+import java.awt.*; 
+
+public class WelcomeScreen extends JFrame implements Runnable {
+    
+    Thread th;
+    WelcomeScreen() {    // constructor 
+        
+        ImageIcon ic1= new ImageIcon(ClassLoader.getSystemResource("icons/new image.jpg")); 
+        Image ic2=ic1.getImage().getScaledInstance(925,800,Image.SCALE_DEFAULT); 
+        ImageIcon ic3=new ImageIcon(ic2);
+        JLabel theimage= new JLabel(ic3);
+        add(theimage); 
+        
+        th=new Thread(this); 
+        th.start(); 
+        setVisible(true); 
+        
+        int n=1;
+        
+        for(int i=2; i<=700; i+=5,n+=1){ 
+        setLocation(750-((i)/2),450-(i/2)); 
+        setSize(i+2*n,i+n/2); 
+        
+        try{
+            
+            Thread.sleep(10);
+        } catch(Exception e) {} 
+    }
+    }
+    public void run() { 
+        try {
+            
+            Thread.sleep(8000);
+            setVisible(false); 
+            
+            new SignIn();
+          } catch(Exception e) { 
+    }
+    }
+    public static void main (String[] args) { 
+        new WelcomeScreen() ; 
+    }
+}    
+        
